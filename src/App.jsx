9b2788ffc04d18195c5d5633690281403b96b12e
Dev-Ruqoyah <hamzatrukayatabiodun02@gmail.com>
2024-12-24 09:51:44 -0800

    Introduction to Array

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,36 @@
 import React, { useState } from "react";
+const App = () =>{
+  const [name,setName] = useState("")
+  const myFunc2 = (e) =>{
+    setName(e.target.value)
+  }
+  const[num,setNum] = useState("")
+  const myFunc = (e,val) =>{
+    console.log(e,val);
+    let a = val + 1
+    setNum(a)
+    
+  }
 
-const App = () => {
-  const [num,setNum] = useState("0")
-  const handleNum = (number) =>{
-    if(num === "0"){
-      setNum(number)
-    }else{
-      setNum(num + number)
-    }
-   
-   
+  const [names,setNames] = useState([]);
+  const getName = () =>{
+    setNames([...names,name])
   }
-  const actionNum = () =>{
-    let calculatedValues = eval(num)
-    setNum(calculatedValues)
-    // setNum(num act num )
-  }
-  const clearScreen = () =>{
-    setNum("0")
-  }
-  const removeLast = () =>{
-    if(num.length === 1){
-      setNum("0")
-    }else{
-      let result = num.slice(0,-1)
-      setNum(result)
-    }
+
   
-  }
- 
-  return (
+  return(
     <>
-      <div className="container">
-        <div className="calculator">
-          <div className="screen" >
-            <p>{num}
-            </p>
-          </div>
-          <div className="content">
-            <div className="one">
-              <button className="action" onClick={clearScreen}>AC</button>
-              <button className="action" onClick={removeLast}>Del</button>
-              <button className="action">%</button>
-              <button className="num"  onClick={() =>{handleNum(7)}}>7</button>
-              <button className="num" onClick={() =>{handleNum(8)}} >8</button>
-              <button className="num" onClick={() =>{handleNum(9)}}>9</button>
-              <button className="num" onClick={() =>{handleNum(4)}}>4</button>
-              <button className="num" onClick={() =>{handleNum(5)}}>5</button>
-              <button className="num" onClick={() =>{handleNum(6)}}>6</button>
-              <button className="num" onClick={() =>{handleNum(1)}}>1</button>
-              <button className="num" onClick={() =>{handleNum(2)}}>2</button>
-              <button className="num" onClick={() =>{handleNum(3)}}>3</button>
-              <button className="num nu0" onClick={() =>{handleNum(0)}}>0</button>
-              <button className="num" onClick={() =>{handleNum(".")}}>.</button>
-            </div>
-            <div className="two">
-            <button className="num1"  onClick={() =>{handleNum("/")}}>/</button>
-            <button className="num1"  onClick={() =>{handleNum("*")}}>*</button>
-            <button className="num1"  onClick={() =>{handleNum("-")}}>-</button>
-            <button className="num1"  onClick={() =>{handleNum("+")}}>+</button>
-            <button className="num1" onClick={actionNum} >=</button>
-
-            </div>
-          </div>
-        </div>
-      </div>
+       <button onClick={(e) =>myFunc(e,5)}>Click me</button> 
+      <p>{num}</p>
+      <input type="text" placeholder="Type a name" onChange={(e) =>myFunc2(e)} />
+      <p>{name}</p>
+      <button onClick={getName}>Get name</button>
+      <p>{names.map(name =>(
+        <li>{name}</li>
+      ))}</p>
 
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/calculator.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+const Calculator = () => {
+  const [num,setNum] = useState("")
+  const handleNum = (number) =>{
+    // setNum(num + number)
+
+    if(num === "0"){
+      setNum(number)
+    }else{
+      setNum(num + number)
+    }
+   
+   
+  }
+  const actionNum = () =>{
+    let calculatedValues = eval(num)
+    setNum(calculatedValues)
+    // setNum(num act num )
+  }
+  const clearScreen = () =>{
+    setNum("")
+  }
+  const removeLast = () =>{
+    if(num.length === 1){
+      setNum("")
+    }else{
+      let result = num.slice(0,-1)
+      setNum(result)
+    }
+  
+  }
+ 
+  return (
+    <>
+      <div className="container">
+        <div className="calculator">
+          <div className="screen" >
+            <p>{num}
+            </p>
+          </div>
+          <div className="content">
+            <div className="one">
+              <button className="action" onClick={clearScreen}>AC</button>
+              <button className="action" onClick={removeLast}>Del</button>
+              <button className="action"  onClick={() =>{handleNum("%")}}>%</button>
+              <button className="num"  onClick={() =>{handleNum(7)}}>7</button>
+              <button className="num" onClick={() =>{handleNum(8)}} >8</button>
+              <button className="num" onClick={() =>{handleNum(9)}}>9</button>
+              <button className="num" onClick={() =>{handleNum(4)}}>4</button>
+              <button className="num" onClick={() =>{handleNum(5)}}>5</button>
+              <button className="num" onClick={() =>{handleNum(6)}}>6</button>
+              <button className="num" onClick={() =>{handleNum(1)}}>1</button>
+              <button className="num" onClick={() =>{handleNum(2)}}>2</button>
+              <button className="num" onClick={() =>{handleNum(3)}}>3</button>
+              <button className="num nu0" onClick={() =>{handleNum(0)}}>0</button>
+              <button className="num" onClick={() =>{handleNum(".")}}>.</button>
+            </div>
+            <div className="two">
+            <button className="num1"  onClick={() =>{handleNum("/")}}>/</button>
+            <button className="num1"  onClick={() =>{handleNum("*")}}>*</button>
+            <button className="num1"  onClick={() =>{handleNum("-")}}>-</button>
+            <button className="num1"  onClick={() =>{handleNum("+")}}>+</button>
+            <button className="num1" onClick={actionNum} >=</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default Calculator
