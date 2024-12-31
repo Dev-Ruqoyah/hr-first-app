5ead1258ba706ec404178aa827c6a8fd273aaaf4
Dev-Ruqoyah <hamzatrukayatabiodun02@gmail.com>
2024-12-31 15:25:22 +0100

    Useeffect with countDown

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./component/Header";
 import Table from "./component/Table";
 import ApiFetch from "./component/ApiFetch";
 import ApiFetch2 from "./component/ApiFetch2";
+import CountDown from "./component/CountDown";
 import ApiFetch3 from "./component/ApiFetch3";
 // import 
 const App = () =>{
@@ -13,7 +14,8 @@ const App = () =>{
       {/* <Header /> */}
       {/* <Table/> */}
       {/* <ApiFetch3/> */}
-      <ApiFetch2/>
+      {/* <ApiFetch2/> */}
+      <CountDown/>
      
      
     </>
---
+++ src/component/CountDown.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+const CountDown = () => {
+    const[timeLap,setTime] = useState("")
+    const [det,setDet] = useState("")
+
+    useEffect(()=>{
+        let myInterval;
+        if(det>0){
+            myInterval = setInterval(() => {
+                setDet(det-1)
+            }, 1000);
+        }
+      
+
+        return()=>{
+            clearInterval(myInterval)
+        }
+    },[det])
+   
+              
+    
+    return ( 
+        <>
+            <input type="text" onInput={(e)=>setTime(e.target.value)} />
+            <button onClick={()=>setDet(timeLap)}>Fetch</button>
+            <p>{det}</p>
+        </>
+     );
+}
+ 
+export default CountDown;
