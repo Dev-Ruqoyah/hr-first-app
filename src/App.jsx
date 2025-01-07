ed3554bc0e2061826a9275784ea616bf85fa0fa8
Dev-Ruqoyah <hamzatrukayatabiodun02@gmail.com>
2025-01-07 22:54:52 +0100

    reduxjs toolkit and react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,44 @@ import Kenny from "./component/Kenny";
 import Form from "./component/Form";
 import { AgeContext } from "./component/context";
 import Details from "./component/Details";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+
+
+let personSlice = createSlice({
+  name:"person ",
+  initialState:{name:"Taye",dept: "computer science",age:12},
+  reducers:{
+    setName: (state)=>{
+      state.name = "Kenny"
+    },
+    setDept: (state)=>{
+      state.dept = "computer Engineering"
+    },
+    setAge:(state,action)=>{
+      state.age = action.payload;
+    }
+  }
+})
+let store = configureStore({
+  reducer:{
+    person:personSlice.reducer
+  }
+})
+export const {setName,setDept,setAge} = personSlice.actions
 const App = () => {
-  const [age, setAge] = useState(10);
-  const increment = () => {
-    setAge(age + 1);
-  };
+  // const [age, setAge] = useState(10);
+  // const increment = () => {
+  //   setAge(age + 1);
+  // };
+
+ 
   return (
     <>
       <NavBar />
-      <AgeContext.Provider value={{ myAge: age ,increments:increment}}>
+      {/* <AgeContext.Provider value={{ myAge: age ,increments:increment}}> */}
+      <Provider store={store}>
         <Routes>
           <Route path="/" element={<Calculator />} />
           <Route path="/contact/:username?/:id?" element={<Contact />} />
@@ -35,7 +64,8 @@ const App = () => {
 
           <Route path="/details" element={<Details />} />
         </Routes>
-      </AgeContext.Provider>
+        </Provider>
+      {/* </AgeContext.Provider> */}
     </>
   );
 };
--- src/component/Details.jsx
+++ src/component/Details.jsx
@@ -1,11 +1,24 @@
-import { useContext } from "react";
-import { AgeContext } from "./context";
+import { useContext, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setAge } from "../App";
+// import { AgeContext } from "./context";
 
 const Details = () => {
-    const {myAge} = useContext(AgeContext)
+    // useSelector to select the values
+    // useDispatch to change the values
+    // const {myAge} = useContext(AgeContext)
+    const[newName,setNewName] = useState("")
+    let {age} = useSelector(state=>state.person)
+    let dispatch = useDispatch()
+    console.log(age);
+   let myfunc = ()=>{
+        dispatch(setAge(newName))
+    }
     
     return ( <>
-        <p>I am {myAge} years old</p>
+        <p>I am years {age} old</p>
+        <input type="text" onChange={(e)=>setNewName(e.target.value)} />
+        <button onClick={myfunc}>Grow</button>
     </> );
 }
  
